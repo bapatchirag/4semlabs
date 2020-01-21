409210dc5c3d77f14d531c3c4d3c0c44d60bbbac
Chirag Bapat <43720649+bapatchirag@users.noreply.github.com>
2020-01-21 13:38:45 +0530

    Update average.s

--- mces/average.s
+++ mces/average.s
@@ -1,17 +1,19 @@
 	AREA RESET, CODE
 	ENTRY
 	
-	LDR R0, =NUMS
-	LDR R1, [R0]
-	MOV R2, #5
-	MOV R3, #0
-	MOV R4, #0
+	LDR R0, =NUMS	; Address of list of numbers
+	LDR R1, [R0]	; First number
+	MOV R2, #5		; Number of numbers in list
+	MOV R3, #0		; Counter
+	MOV R4, #0		; Average to be stored
 	
+; Calculates sum of numbers in list
 SUM ADD R5, R5, R1
 	ADD R3, R3, #1
 	CMP R3, R2
 	BLT SUM
 	
+; Division = Repeated subtraction
 DIVIDE SUB R5, R5, R2
 	ADD R4, R4, #1
 	CMP R5, #0
