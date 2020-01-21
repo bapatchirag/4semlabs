49e42bdb4e4c9ef1aedcedf802bd3394a8a06aa9
Chirag Bapat <43720649+bapatchirag@users.noreply.github.com>
2020-01-21 20:24:53 +0530

    Added comments to average.s

--- mces/average.s
+++ mces/average.s
@@ -1,23 +1,27 @@
 	AREA RESET, CODE
 	ENTRY
 	
-	LDR R0, =LIST
-	MOV R1, #5
-	MOV R2, R1
-	LDR R3, [R0]
+	LDR R0, =LIST	; Address of first element in list
+	MOV R1, #5		; Number of elements in list
+	MOV R2, R1		; Copy of R1
+	LDR R3, [R0]	; First element of list
 	
+; Sums up all numbers in list
 LOOP ADD R4, R4, R3
 	LDR R3, [R0, #4]!
 	SUB R2, R2, #1
 	CMP R2, #0
 	BGT LOOP
-	
+
+; Division = Repeated Subtraction
 DIVIDE SUB R4, R4, R1
 	ADD R5, R5, #1
 	CMP R4, #0
 	BGT DIVIDE
-	
+
+; Stops execution of code
 STOP B STOP
 
+; Test list of numbers
 LIST DCD &2, &1, &2, &3, &2
 	END
