08968bcf11f38f6a6bdc6bc77a1154cf003a7527
Chirag Bapat <43720649+bapatchirag@users.noreply.github.com>
2020-01-21 19:38:48 +0530

    Added comments to factorial.s

--- mces/factorial.s
+++ mces/factorial.s
@@ -1,15 +1,18 @@
 	AREA RESET, CODE
 	ENTRY
 	
-	LDR R0, NUM
-	MOV R1, #1
+	LDR R0, NUM		; Number for which factorial is to be calculated
+	MOV R1, #1		; Factorial to be stored here
 	
+; Sub-routine FACT called iteratively until R0 becomes 0
 LOOP BL FACT
 	CMP R0, #0
 	BGT LOOP
 
+; Stops execution of program
 STOP B STOP
 
+; Sub-routine call which multiplies R0 and value at R1
 FACT MUL R1, R0, R1
 	SUB R0, R0, #1
 	BX LR
