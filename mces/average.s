7aa4322aa5658191d4a11dd7d4ada08a9fc56d93
Chirag Bapat <43720649+bapatchirag@users.noreply.github.com>
2020-01-21 20:31:10 +0530

    Update average.s

--- mces/average.s
+++ mces/average.s
@@ -2,12 +2,12 @@
 	ENTRY
 	
 	LDR R0, =LIST	; Address of first element in list
-	MOV R1, #5		; Number of elements in list
-	MOV R2, R1		; Copy of R1
+	MOV R1, #5	; Number of elements in list
+	MOV R2, R1	; Copy of R1
 	LDR R3, [R0]	; First element of list
 	
 ; Sums up all numbers in list
-LOOP ADD R4, R4, R3
+LOOP ADD R4, R4, R3	; R4 = Sum of list elements
 	LDR R3, [R0, #4]!
 	SUB R2, R2, #1
 	CMP R2, #0
@@ -15,7 +15,7 @@ LOOP ADD R4, R4, R3
 
 ; Division = Repeated Subtraction
 DIVIDE SUB R4, R4, R1
-	ADD R5, R5, #1
+	ADD R5, R5, #1	; Quotient of R4 / R1
 	CMP R4, #0
 	BGT DIVIDE
 
