cd6927be7b86b143e5714e025eddd9396408bd36
Chirag Bapat <43720649+bapatchirag@users.noreply.github.com>
2020-01-21 12:58:41 +0530

    Calculates GCD of two numbers

--- mces/gcd.s
+++ mces/gcd.s
@@ -1,10 +1,11 @@
 	AREA RESET, CODE
 	ENTRY
 	
-	LDR R0, =NUMS
-	LDR R1, [R0]
-	LDR R2, [R0, #4]
+	LDR R0, =NUMS		; Address of first number (both numbers stored together)
+	LDR R1, [R0]		; Stores first number (and gcd)
+	LDR R2, [R0, #4]	; Stores second number (and gcd)
 	
+; Calculates GCD of values in R1 and R2 using Euclid's algorithm
 GCD CMP R1, R2
 	SUBGT R1, R1, R2
 	SUBLT R2, R2, R1
