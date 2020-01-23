ab8b0e8b72db760f900a93c62964beb8a66e9796
Chirag Bapat <43720649+bapatchirag@users.noreply.github.com>
2020-01-23 13:44:21 +0530

    Super class

---
+++ oops/packages/CSE/Thirdsem.java
@@ -0,0 +1,10 @@
+package CSE;
+
+public class Thirdsem
+{
+    // Member functions
+    public void Welcomemsg()
+    {
+        System.out.println("Welcome to CSE dept - 3rd sem young budding Engineers");
+    }
+}
